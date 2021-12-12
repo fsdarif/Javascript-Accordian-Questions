4e382b5ffed21ecf129964b827c6fabfa96a032c
Ahnaf Arif <2080arif@gmail.com>
2021-12-13 05:01:37 +0600

    comment

---
+++ app.js
@@ -0,0 +1,28 @@
+//using selectors inside the element
+// traversing the dom
+
+const questions = document.querySelectorAll('.question');
+questions.forEach((question) => {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', () => {
+        questions.forEach ((item) => {
+            if (item !== question)  {
+                item.classList.remove('show-text')
+            }
+        })
+        question.classList.toggle('show-text')
+    })
+})
+
+
+/* alternative */
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//     btn.addEventListener("click", (e) => {
+//         question = e.currentTarget.parentNode.parentNode;
+//         question.classList.toggle("show-text")
+//         console.log(question)
+//     })
+// })
